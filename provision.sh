141578c72e6898252d172e3d560cee20fa115ed9
James Kulba <jkulba@users.noreply.github.com>
2018-04-18 12:37:12 -0700

    Comment out dependencies

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ echo "Updating system..."
 apt-get -y update
 
 echo "Installing dependencies for PiShrink environment..."
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev openssl bzip2 git
+#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev openssl bzip2 git
 
 # https://github.com/Drewsif/PiShrink
 mkdir /home/vagrant/Downloads
